418adad55001a9d20388a36cc9cefef62cd1a01c
Yoto Kim <yongtokim1215@gmail.com>
2023-04-19 15:40:03 -0700

    Reverse Methods Fixed
    Fixed reverse  methods

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,8 +4,12 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    int[] copy = new int[arr.length];
+    for (int i=0; i<arr.length; i++){
+      copy[i] = arr[i];
+    }
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      arr[i] = copy[arr.length - i - 1];
     }
   }
 
@@ -14,9 +18,9 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlaceNoWork(){
+    int[] i = {50, 51, 52};
+    ArrayExamples.reverseInPlace(i);
+    assertArrayEquals(new int[]{52, 51, 50}, i);
+  }
+
+  @Test
+  public void testReversedNoWork(){
+    int[] m = {70, 71, 72};
+    assertArrayEquals(new int[]{72, 71, 70}, ArrayExamples.reversed(m));
+  }
 }
